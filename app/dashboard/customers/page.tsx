f43a95e9380979af9d5359924cc607d4e3334a13
Aditya Raj <adityarj729@gmail.com>
2024-03-04 19:05:47 +0530

    Initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function customers(){
+    return <div>
+        <p>Customers page</p>
+    </div>
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function invoice(){
+    return <div>
+        <p>Invoices Page</p>
+    </div>
+}
