2f70c0f143462e2a39ee39714cdd6b75abde304f
Aditya Raj <adityarj729@gmail.com>
2024-03-06 16:15:33 +0530

    noStore change

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,13 +16,13 @@ export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
+  noStore();
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
-    noStore();
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
